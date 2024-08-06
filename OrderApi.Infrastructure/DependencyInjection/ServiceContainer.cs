6387e8da1e4b81a6b5e6adf527026d910e72b0d9
MrEshboboyev <mreshboboyev@gmail.com>
2024-08-06 14:20:07 +0500

    Create OrderApi.Infrastructure/DependencyInjection/ServiceContainer.cs : AddInfrastructure(), UseInfrastructurePolicy();

---
+++ OrderApi.Infrastructure/DependencyInjection/ServiceContainer.cs
@@ -0,0 +1,40 @@
+﻿using eCommerce.SharedLibrary.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OrderApi.Application.Interfaces;
+using OrderApi.Infrastructure.Data;
+using OrderApi.Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderApi.Infrastructure.DependencyInjection
+{
+    public static class ServiceContainer
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration config)
+        {
+            // Add Database connectivity
+            // Add Authentication scheme
+            SharedServiceContainer.AddSharedService<OrderDbContext>(services, config, config["MySerilog:FileName"]!);
+
+            // Create Dependency Injection
+            services.AddScoped<IOrder, OrderRepository>();
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseInfrastructurePolicy(this IApplicationBuilder app)
+        {
+            // Register middleware such as:
+            // Global exception -> handle external errors
+            // ListenToOnlyApiGateway Only -> block all outsiders calls
+            SharedServiceContainer.UseSharedPolicies(app);
+
+            return app;
+        }
+    }
+}
